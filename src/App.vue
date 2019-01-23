bcde97844a1b48991aba0b769c75f9a7c220d52a
mingql <mingqili0@163.com>
2019-01-24 00:38:14 +0800

    aa

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <app-header></app-header>
+    </div>
+  </div>
+</template>
+
+<script>
+  import  Header from "./components/Header"
+export default {
+  name: 'app',
+  components:{
+    "app-header":Header
+  }
+}
+</script>
